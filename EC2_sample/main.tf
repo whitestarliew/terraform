15a2c5c0ac01d751e8e39253259d282347c9bccb
whitestarliew <whitestarliew@gmail.com>
2023-06-27 17:07:45 +0800

    tgtrshyh

--- EC2_sample/main.tf
+++ EC2_sample/main.tf
@@ -72,3 +72,7 @@ resource "aws_instance" "bastion_host" {
 output "bastion_public_ip" {
   value = aws_instance.bastion_host.public_ip
 }
+
+output "nat_gateway_id" {
+  value = aws_nat_gateway.nat_gateway.id  
+}
