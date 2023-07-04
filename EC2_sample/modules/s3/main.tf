12f43fbc13e2a551e1589dc245f6050bcb488bc0
whitestarliew <whitestarliew@gmail.com>
2023-07-04 17:20:30 +0800

    modules nat gateway

--- EC2_sample/modules/s3/main.tf
+++ EC2_sample/modules/s3/main.tf
@@ -1,6 +1,6 @@
 # s3-bucket-module/main.tf
 
-resource "aws_s3_bucket" "example_bucket" {
+resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
   acl    = "private"
   # other bucket configuration...
