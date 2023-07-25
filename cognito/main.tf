d1f88daa11f44b6316535d5088805c5294b7111a
whitestarliew <whitestarliew@gmail.com>
2023-07-25 22:00:23 +0800

    new

--- cognito/main.tf
+++ cognito/main.tf
@@ -2,12 +2,26 @@ provider "aws" {
   region = var.aws_regions
 }
 
-resource "aws_cognito_user_pool" "pool" {
-  name = "mypool"
+resource "aws_cognito_user_pool" "test_user_pool" {
+  name = "user1_pool"
+
+  schema {
+    name                     = "foo"
+    attribute_data_type      = "Boolean"
+    mutable                  = false
+    required                 = false
+    developer_only_attribute = false
+    string_attribute_constraints {}
+  }
+}
+
+resource "aws_cognito_user" "example" {
+  user_pool_id = aws_cognito_user_pool.test_user_pool
+  username     = "john_doe"
 }
 
 output "user_pool_id" {
-  value = aws_cognito_user_pool.example_user_pool.id
+  value = aws_cognito_user_pool.test_user_pool.id
 }
 
 
