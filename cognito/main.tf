becd1c3aefafb936194c2d3f0015fed712d76586
whitestarliew <whitestarliew@gmail.com>
2023-07-25 20:25:23 +0800

    terraform

--- cognito/main.tf
+++ cognito/main.tf
@@ -1,32 +1,13 @@
 provider "aws" {
-  region = var.aws_region # Change this to your desired AWS region
-}
-resource "aws_cognito_user_pool" "example_user_pool" {
-  name = "test-user-pool"
-  # Add more configuration options as needed
-}
-resource "aws_cognito_identity_pool" "example_identity_pool" {
-  identity_pool_name = "test-identity-pool"
-  allow_unauthenticated_identities = false
-
-locals {
-  valid_client_id = replace(aws_cognito_user_pool.example_user_pool.id, "[^a-zA-Z0-9]", "_")
+  region = var.aws_regions
 }
 
-  # Link to the user pool created above
-  cognito_identity_providers {
-    client_id               = local.valid_client_id
-    provider_name           = aws_cognito_user_pool.example_user_pool.endpoint
-    server_side_token_check = false
-  }
-
-  # Add more configuration options as needed
+resource "aws_cognito_user_pool" "pool" {
+  name = "mypool"
 }
 
 output "user_pool_id" {
   value = aws_cognito_user_pool.example_user_pool.id
 }
 
-output "identity_pool_id" {
-  value = aws_cognito_identity_pool.example_identity_pool.id
-}
+
--- cognito/variables.tf
+++ cognito/variables.tf
@@ -1,5 +1,9 @@
-variable "aws_region" {
-  description = "This is for AWS Region"
-  type        = string
-  default     = "us-east-1"
+variable "aws_regions" {
+  description = "The desired AWS region."
+  default     = "us-east-1"  # Replace this with your preferred default region
 }
+
+# variable "valid_client_id" {
+#   description = "Computed valid client_id"
+#   default     = replace(aws_cognito_user_pool.example_user_pool.id, "[^a-zA-Z0-9]", "_")
+# }
