abbdb469e46039aca83dd38abd6757f6581c392e
whitestarliew <whitestarliew@gmail.com>
2023-07-25 19:45:19 +0800

    cognito

--- cognito/main.tf
+++ cognito/main.tf
@@ -1,19 +1,21 @@
 provider "aws" {
-  region = "us-east-1" # Change this to your desired AWS region
+  region = var.aws_region # Change this to your desired AWS region
 }
-
 resource "aws_cognito_user_pool" "example_user_pool" {
-  name = "example-user-pool"
+  name = "test-user-pool"
   # Add more configuration options as needed
 }
-
 resource "aws_cognito_identity_pool" "example_identity_pool" {
-  identity_pool_name = "example-identity-pool"
+  identity_pool_name = "test-identity-pool"
   allow_unauthenticated_identities = false
+
+locals {
+  valid_client_id = replace(aws_cognito_user_pool.example_user_pool.id, "[^a-zA-Z0-9]", "_")
+}
 
   # Link to the user pool created above
   cognito_identity_providers {
-    client_id               = aws_cognito_user_pool.example_user_pool.id
+    client_id               = local.valid_client_id
     provider_name           = aws_cognito_user_pool.example_user_pool.endpoint
     server_side_token_check = false
   }
---
+++ cognito/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {
+  description = "This is for AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
