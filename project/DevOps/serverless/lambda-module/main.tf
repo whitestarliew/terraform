fe037ca3d9ae3fc838d58542e5f608e7edd74ec8
whitestarliew <whitestarliew@gmail.com>
2024-01-02 16:21:01 +0800

    update

--- project/DevOps/serverless/lambda-module/main.tf
+++ project/DevOps/serverless/lambda-module/main.tf
@@ -1,10 +1,12 @@
-terraform {
-  
-  
-}
+
 
 provider "aws" {
   region = var.aws_region 
+  # Make it faster by skipping something
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
 }
 
 
