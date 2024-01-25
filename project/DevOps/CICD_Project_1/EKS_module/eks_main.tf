2ce9086b1045e36ac6a2c09100d079c4ef0c8ef5
whitestarliew <whitestarliew@gmail.com>
2024-01-26 00:29:14 +0800

    update

--- project/DevOps/CICD_Project_1/EKS_module/eks_main.tf
+++ project/DevOps/CICD_Project_1/EKS_module/eks_main.tf
@@ -16,3 +16,17 @@ resource "aws_eks_cluster" "testing_eks" {
     subnet_ids = [module.VPC_ID.public_subnet_id_output, module.VPC_ID.public_subnet_id_2_output]
   }
 }
+
+resource "aws_eks_fargate_profile" "fargate_sample" {
+  fargate_profile_name = "testing-fargate-profile"
+  podpod_execution_role_arn = ""
+  selector {
+    match_labes = {
+      "app" = "sample-app"
+    }
+  }
+  subnet_ids = [module.VPC_ID.public_subnet_id_output, module.VPC_ID.public_subnet_id_2_output]
+  tags = {
+    Name = "sample-fargate-tag"
+  }
+}
