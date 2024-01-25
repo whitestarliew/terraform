9fd4c2b35702102f7fefe917dc26e0a8267117b7
whitestarliew <whitestarliew@gmail.com>
2024-01-25 20:07:32 +0800

    update

--- Packer/installation1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo yum -y update
-sudo yum install docker
-exit 0
--- project/DevOps/EKS_Project_1/EKS_module/main.tf
+++ project/DevOps/EKS_Project_1/EKS_module/main.tf
@@ -5,21 +5,3 @@ terraform {
 provider "aws" {
     required_version = ">=3.0.0"
 }
-
-data "aws_vpc" "" {
-  type        = "zip"
-  output_path = "${path.module}/files/output.zip"
-
-  source {
-    content  = ""
-    filename = ""
-  }
-}
-
-
-resource "aws_eks_cluster" "example" {
-  depends_on = [aws_cloudwatch_log_group.example]
-
-  enabled_cluster_log_types = ["api", "audit"]
-  name                      = var.cluster_names
-}
