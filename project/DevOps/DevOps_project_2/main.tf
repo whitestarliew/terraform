e802fb85ebae6d0d7684476a8199c77bbba890bb
whitestarliew <whitestarliew@gmail.com>
2024-01-28 00:37:51 +0800

    update

---
+++ project/DevOps/DevOps_project_2/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.15" 
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region 
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-235123151010121231532"
+
+  tags = {
+    Name        = "For the testing"
+    Environment = "Dev"
+  }
+}
