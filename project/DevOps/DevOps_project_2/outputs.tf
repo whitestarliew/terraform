dfe4adaf961e702097d41561cab35846ecbabaf9
whitestarliew <whitestarliew@gmail.com>
2024-06-04 10:19:15 +0800

    update

--- project/DevOps/DevOps_project_2/outputs.tf
+++ project/DevOps/DevOps_project_2/outputs.tf
@@ -2,3 +2,8 @@ output "S3_bucket" {
     description = "Output for S3"
     value = aws_s3_bucket.regov_example  
 }
+
+output "code_commit_output" {
+    description = "Output for code commit "
+    value = aws_codecommit_repository.test 
+}
